bd007ec386046202da4befb66bd9084fefa4c7f7
Ewout Phlips <>
2018-03-05 11:03:02 +0100

    Inhoud Klasse Doelstelling

--- src/java/model/Doelstelling.java
+++ src/java/model/Doelstelling.java
@@ -1,17 +1,50 @@
+
 package model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
+ * Klasse Doelstelling:
+ * Deze klasse beschrijft een wat een doelstelling inhoud. 
+ * 
  * @author Ewout Phlips
  */
+
 public class Doelstelling {
-    // TEST
-   
-    // TEst 22
+    
+    private int doelstellingID; // ID van de doelstelling in de DataBase
+    private String naam; // Naam van de doelstelling.
+    private String beschrijving; // Beschrijving van de doelstellig.
+    private boolean kerndoelstelling; // Kerndoelstelling. Ja of Nee
+    
+    public int getDoelstellingID() {
+        return doelstellingID;
+    }
+    
+    public void setDoelstellingID(int doelstellingID) {
+        this.doelstellingID = doelstellingID;
+    }
+    
+    public String getNaam() {
+        return naam;
+    }
+    
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+    
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
+    
+    public boolean getKerndoelstelling() {
+        return kerndoelstelling;
+    }
+    
+    public void setKerndoelstelling(boolean kerndoelstelling) {
+        this.kerndoelstelling = kerndoelstelling;
+    }
+
 }
