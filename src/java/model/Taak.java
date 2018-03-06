335d21d0323f7c66a43eeeb3ed30e7cf3a315cac
CURSIST <CURSIST@KL103-71469DOC.curlokeren>
2018-03-06 09:59:29 +0100

    Taak en TaakDAO aangepast

--- src/java/model/Taak.java
+++ src/java/model/Taak.java
@@ -1,21 +1,46 @@
 package model;
         
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Aaron
+ * 
+ * Taak is een praktische opdracht die tijdens de les
+ * wordt gegeven en vervult 
+ * daarmee (gedeeltelijk) een doelstelling.
+ * 
  */
 public class Taak {
     
     private int taakID;
     private String naam;
     private String beschrijving;
+
     
-    // Toevoegen
+
+    public int getTaakID() {
+        return taakID;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+
+    public void setTaakID(int taakID) {
+        this.taakID = taakID;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
+    
+   
    
 }
--- src/java/model/TaakDAO.java
+++ src/java/model/TaakDAO.java
@@ -1,29 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
- * @author Aaron
- * hier komen sql query's
+ * @author Aaron Deze klasse is verantwoordelijk voor het ophalen, wijzigen en
+ * verwijderen van taken in de database.
  */
 public class TaakDAO {
-    private void TaakAanmaken(Gebruiker gebruiker){
-        
-        String sql = "INSERT INTO Taken(taakID, naam, beschrijving) VALUES(?,?,?)";
-        // taak aanmaken door naam en beschrijving in te vullen
+
+    private void TaakAanmaken(Taak taak) {
+
+        //connectie maken met database
+        Connection connectie = null;
+
+        //insertquery
+        String sql = "INSERT INTO Taken "
+                + "(naam,beschrijving) VALUES (?,?)";
+
+        //opslaan in database
+        try {
+            PreparedStatement statement = connectie.prepareStatement(sql);
+            statement.setString(1, taak.getNaam());
+            statement.setString(2, taak.getBeschrijving());
+            statement.execute();
+            statement.close();
+            connectie.close();
+
+        } catch (SQLException e) {
+
+        }
     }
-    private void TaakAanpassen(Gebruiker gebruiker){
-        
-        String sql = "UPDATE Taken SET naam ='?' where taakID ='?'";
-        // taak aanpassen door de taakID in te vullen en de juiste naam
+
+    private void TaakAanpassen(Taak taak) {
+
+        //connectie maken met database
+        Connection connectie = null;
+        connectie = ConnectionManager.getConnection();
+
+        //insertquery
+        String sql = "UPDATE Taken "
+                + " set naam =  " + taak.getNaam() + ", beschrijving= "
+                + taak.getBeschrijving() + " where taakID= "
+                + taak.getTaakID();
+
+        //opslaan in database
+        try {
+            Statement statement = connectie.createStatement();
+            statement.executeUpdate(sql);
+            statement.close();
+            connectie.close();
+
+        } catch (SQLException e) {
+
+        }
+
     }
-    private void TaakVerwijderen(Gebruiker gebruiker){
-        
-        String sql = "DELETE FROM Taken where taakID='?'";   
-        // taak verwijderen door de taakID in te vullen
-}
+
+    private void TaakVerwijderen(Taak taak) {
+
+        //connectie maken met database
+        Connection connectie = null;
+
+        //insertquery
+        String sql = "DELETE FROM Taken "
+                + " where taakID= " + taak.getTaakID();
+
+        //opslaan in database
+        try {
+            PreparedStatement statement = connectie.prepareStatement(sql);
+            statement.execute();
+            statement.close();
+            connectie.close();
+
+        } catch (SQLException e) {
+
+        }
+
+    }
 }
