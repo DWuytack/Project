958d8629e563c9646f91dbb9d9f613682904b4e3
CURSIST <CURSIST@KL103-71476.curlokeren>
2018-03-06 10:19:40 +0100

    Doelstelling en DAO aangepast

--- src/java/model/Doelstelling.java
+++ src/java/model/Doelstelling.java
@@ -1,48 +1,46 @@
-
 package model;
 
 /**
- * Klasse Doelstelling:
- * Deze klasse beschrijft een wat een doelstelling inhoud. 
- * 
+ * Klasse Doelstelling: Een doelstelling is een verzameling taken die
+ * bijeenhoort.
+ *
  * @author Ewout Phlips
  */
-
 public class Doelstelling {
-    
+
     private int doelstellingID; // ID van de doelstelling in de DataBase
     private String naam; // Naam van de doelstelling.
     private String beschrijving; // Beschrijving van de doelstellig.
     private boolean kerndoelstelling; // Kerndoelstelling. Ja of Nee
-    
+
     public int getDoelstellingID() {
         return doelstellingID;
     }
-    
+
     public void setDoelstellingID(int doelstellingID) {
         this.doelstellingID = doelstellingID;
     }
-    
+
     public String getNaam() {
         return naam;
     }
-    
+
     public void setNaam(String naam) {
         this.naam = naam;
     }
-    
+
     public String getBeschrijving() {
         return beschrijving;
     }
-    
+
     public void setBeschrijving(String beschrijving) {
         this.beschrijving = beschrijving;
     }
-    
+
     public boolean getKerndoelstelling() {
         return kerndoelstelling;
     }
-    
+
     public void setKerndoelstelling(boolean kerndoelstelling) {
         this.kerndoelstelling = kerndoelstelling;
     }
--- src/java/model/DoelstellingDAO.java
+++ src/java/model/DoelstellingDAO.java
@@ -1,15 +1,88 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+
 /**
  * Klasse Doelstelling DataBase Access Object:
+ *
  *
  * @author Ewout Phlips
  */
 public class DoelstellingDAO {
-    
-    // Doelstelling Aanmaken
-    
-    // Doelstelling Verwijderen
-    
-    // Doelstelling Aanpassen
+
+    public void DoelstellingAanmaken(Doelstelling doelstelling) {
+        Connection currentCon = null; 
+        
+        String sqlQuery
+                = "INSERT INTO Doelstellingen (naam, beschrijving, kerndoelstelling)"
+                + " VALUES ('" + doelstelling.getNaam() + "', '"
+                + doelstelling.getBeschrijving() + "', "
+                + doelstelling.getKerndoelstelling() + ");";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+            Statement stmt = currentCon.createStatement();
+            stmt.executeQuery(sqlQuery);
+
+        } catch (SQLException error) {
+            System.out.println("De volgende Query leverde een error op: " + sqlQuery + ".");
+        } finally {
+            try {
+                currentCon.close();
+            } catch (SQLException error) {
+                System.out.println("Error - Kon de connectie niet verbreken na het aanmaken van een doelstelling.");
+            }
+        }
+    }
+
+    public void DoelstellingAanpassen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+
+        String sqlQuery
+                = "UPDATE Doelstellingen"
+                + "SET naam='" + doelstelling.getNaam()
+                + "', beschrijving='" + doelstelling.getBeschrijving()
+                + "', kerndoelstelling='" + doelstelling.getKerndoelstelling()
+                + "' WHERE doelstellingID='" + doelstelling.getDoelstellingID() + "';";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+            Statement stmt = currentCon.createStatement();
+            stmt.execute(sqlQuery);
+
+        } catch (SQLException error) {
+            System.out.println("Error - De volgende Query werkte niet: " + sqlQuery + ".");
+        } finally {
+            try {
+                currentCon.close();
+            } catch (SQLException error) {
+                System.out.println("Error - Kon de connectie niet verbreken na het aanpassen van een doelstelling.");
+            }
+        }
+    }
+
+    public void DoelstellingVerwijderen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+
+        String sqlQuery
+                = "DELETE FROM Doelstellingen WHERE doelstellingID='" + doelstelling.getDoelstellingID() + "';";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+            Statement stmt = currentCon.createStatement();
+            stmt.execute(sqlQuery);
+
+        } catch (SQLException error) {
+            System.out.println("Error - De volgende Query werkte niet: " + sqlQuery + ".");
+        } finally {
+            try {
+                currentCon.close();
+            } catch (SQLException error) {
+                System.out.println("Error - Kon de connectie niet verbreken na het verwijderen van een doelstelling.");
+            }
+        }
+    }
+
 }
