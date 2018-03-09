bfaf480c30387382fa32bba43896dd1bd4218d83
CURSIST <CURSIST@KL103-71469DOC.curlokeren>
2018-03-09 10:50:43 +0100

    overzicht in cursisten.jsp toegevoegd

--- src/java/controller/MenuServlet.java
+++ src/java/controller/MenuServlet.java
@@ -71,6 +71,8 @@ public class MenuServlet extends HttpServlet {
                     response.sendRedirect("Module.jsp");
                     break;
                 case "Overzicht scores":
+                    //laden van de scores uit de database
+                    
                     response.sendRedirect("Score.jsp");
                     break;
                 case "Evaluatieformulieren":
@@ -80,6 +82,7 @@ public class MenuServlet extends HttpServlet {
                     response.sendRedirect("Rapport.jsp");
                     break;
                 case "Type score aanpassen":
+                    //laden van de types scores
                     response.sendRedirect("TypeScore.jsp");
                     break;
             }
