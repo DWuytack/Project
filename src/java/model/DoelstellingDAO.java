1f58cea2fc1932b38ba2bc0e2233949a49c05b41
Ewout Phlips <CURSIST@KL103-71476.curlokeren>
2018-03-23 11:38:46 +0100

    Doelstelling Aangepast naar nieuw formaat.

--- src/java/model/DoelstellingDAO.java
+++ src/java/model/DoelstellingDAO.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
@@ -14,102 +15,19 @@ import java.util.ArrayList;
  */
 public class DoelstellingDAO {
 
-    //Creër een nieuwe doelstelling.
-    public void doelstellingAanmaken(Doelstelling doelstelling) {
-
-        Connection currentCon = null;
-
-        String sqlQuery //Query voor het aanmaken van een doelstelling.
-                = "INSERT INTO Doelstellingen (naam, beschrijving, kerndoelstelling)"
-                + " VALUES ('" + doelstelling.getNaam() + "', '"
-                + doelstelling.getBeschrijving() + "', "
-                + doelstelling.getKerndoelstelling() + ");";
-
-        try { //Maak connectie met de DataBase en probeer de Query uit te voeren.
-            currentCon = ConnectionManager.getConnection();
-            Statement stmt = currentCon.createStatement();
-            stmt.executeQuery(sqlQuery);
-
-        } catch (SQLException error) { //Error boodschap.
-            System.out.println("De volgende Query leverde een error op: " + sqlQuery + ".");
-
-        } finally {
-            try { //Sluit de connectie met de de DataBase.
-                currentCon.close();
-            } catch (SQLException error) {
-                System.out.println("Error - Kon de connectie niet verbreken na het aanmaken van een doelstelling.");
-            }
-        }
-    }
-
-    //Pas een doelstelling aan.
-    public void doelstellingAanpassen(Doelstelling doelstelling) {
-        Connection currentCon = null;
-
-        String sqlQuery //Query voor het aanpassen van een doelstelling.
-                = "UPDATE Doelstellingen"
-                + "SET naam='" + doelstelling.getNaam()
-                + "', beschrijving='" + doelstelling.getBeschrijving()
-                + "', kerndoelstelling='" + doelstelling.getKerndoelstelling()
-                + "' WHERE doelstellingID='" + doelstelling.getDoelstellingID() + "';";
-
-        try { //Maak connectie met de DataBase en probeer de Query uit te voeren.
-            currentCon = ConnectionManager.getConnection();
-            Statement stmt = currentCon.createStatement();
-            stmt.execute(sqlQuery);
-
-        } catch (SQLException error) { //Error boodschap.
-            System.out.println("Error - De volgende Query werkte niet: " + sqlQuery + ".");
-
-        } finally {
-            try { //Sluit de connectie met de DataBase.
-                currentCon.close();
-            } catch (SQLException error) {
-                System.out.println("Error - Kon de connectie niet verbreken na het aanpassen van een doelstelling.");
-            }
-        }
-    }
-
-    //Verwijdert een doelstelling.
-    public void doelstellingVerwijderen(Doelstelling doelstelling) {
-        Connection currentCon = null;
-
-        String sqlQuery //Query voor het verwijderen van een doelstelling.
-                = "DELETE FROM Doelstellingen WHERE doelstellingID='"
-                + doelstelling.getDoelstellingID() + "';";
-
-        try { //Maak connectie met de DataBase en probeer de Query uit te voeren.
-            currentCon = ConnectionManager.getConnection();
-            Statement stmt = currentCon.createStatement();
-            stmt.execute(sqlQuery);
-
-        } catch (SQLException error) { //Error Boodschap
-            System.out.println("Error - De volgende Query werkte niet: " + sqlQuery + ".");
-
-        } finally {
-            try { //Sluit de connectie met de DataBase.
-                currentCon.close();
-            } catch (SQLException error) {
-                System.out.println("Error - Kon de connectie niet verbreken na het verwijderen van een doelstelling.");
-            }
-        }
-    }
-
     //Laad alle Doelstellingen uit de Database.
     public ArrayList<Doelstelling> doelstellingenLaden() {
 
-        Statement stmt = null;
-        Connection currentCon = null;
-        ResultSet rs = null;
-
         ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
-
-        String selectDoelstellingen = "SELECT * FROM Doelstellingen;";
+        Connection currentCon = null;
+        Statement statement = null;
+        ResultSet rs = null;
 
         try {
             currentCon = ConnectionManager.getConnection();
-            stmt = currentCon.createStatement();
-            rs = stmt.executeQuery(selectDoelstellingen);
+            String sql = "SELECT * FROM doelstellingen";
+            statement = currentCon.createStatement();
+            rs = statement.executeQuery(sql);
 
             while (rs.next()) {
                 Doelstelling doelstelling = new Doelstelling();
@@ -120,14 +38,8 @@ public class DoelstellingDAO {
 
                 doelstellingen.add(doelstelling);
             }
+        } catch (SQLException e) {
 
-            currentCon.close();
-
-            return doelstellingen;
-
-        } catch (Exception e) {
-            System.out.println("Error - Kon geen doelstellingen laden.");
-            return null;
         } finally {
             if (rs != null) {
                 try {
@@ -137,19 +49,78 @@ public class DoelstellingDAO {
                 rs = null;
             }
 
-            if (stmt != null) {
+            if (statement != null) {
                 try {
-                    stmt.close();
+                    statement.close();
                 } catch (Exception e) {
+
                 }
 
-                stmt = null;
+                statement = null;
             }
 
             if (currentCon != null) {
                 try {
                     currentCon.close();
+            } catch (SQLException error) {
+                System.out.println("Error - Kon de connectie niet verbreken na het aanmaken van een doelstelling.");
+                }
+
+                currentCon = null;
+            }
+
+        }
+        return doelstellingen;
+    }
+
+    //Creër een nieuwe doelstelling.
+    public void doelstellingAanmaken(Doelstelling doelstelling) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "INSERT INTO doelstellingen (naam, beschrijving, kerndoelstelling)\n"
+                + "VALUES (?, ?, ?)";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+            ps = currentCon.prepareStatement(sql);
+
+            ps.setString(1, doelstelling.getNaam());
+            ps.setString(2, doelstelling.getBeschrijving());
+            if (doelstelling.getKerndoelstelling()) {
+                ps.setInt(3, 1);
+            } else {
+                ps.setInt(3, 0);
+            }
+            ps.executeQuery(sql);
+
+        } catch (SQLException ex) {
+
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+
+                }
+                rs = null;
+            }
+
+            if (ps != null) {
+                try {
+                    ps.close();
                 } catch (Exception e) {
+
+                }
+                ps = null;
+            }
+
+            if (currentCon != null) {
+                try {
+                    currentCon.close();
+                } catch (SQLException e) {
+
                 }
 
                 currentCon = null;
@@ -157,4 +128,104 @@ public class DoelstellingDAO {
         }
 
     }
+
+    //Pas een doelstelling aan.
+    public void doelstellingAanpassen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE doelstellingen SET naam = ?, beschrijving = ?, kerndoelstelling = ? WHERE doelstellingID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+            ps = currentCon.prepareStatement(sql);
+
+            ps.setString(1, doelstelling.getNaam());
+            ps.setString(2, doelstelling.getBeschrijving());
+            if (doelstelling.getKerndoelstelling()) {
+                ps.setInt(3, 1);
+            } else {
+                ps.setInt(3, 0);
+            }
+            ps.setInt(4, doelstelling.getDoelstellingID());
+            ps.executeQuery(sql);
+
+        } catch (SQLException ex) {
+
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+                rs = null;
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+                ps = null;
+            }
+            if (currentCon != null) {
+                try {
+                    currentCon.close();
+                } catch (SQLException e) {
+                }
+                currentCon = null;
+            }
+        }
+
+    }
+
+    //Verwijdert een doelstelling.
+    public void doelstellingVerwijderen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+        String sql
+                = "DELETE FROM doelstellingen WHERE doelstellingID = ?";
+        try {
+            currentCon = ConnectionManager.getConnection();
+            ps = currentCon.prepareStatement(sql);
+
+            ps.setInt(1, doelstelling.getDoelstellingID());
+            ps.executeQuery(sql);
+
+        } catch (SQLException ex) {
+
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+
+                }
+                rs = null;
+            }
+
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+
+                }
+                ps = null;
+            }
+
+            if (currentCon != null) {
+                try {
+                    currentCon.close();
+                } catch (SQLException e) {
+
+                }
+
+                currentCon = null;
+            }
+        }
+
+    }
+
 }
