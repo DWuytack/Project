852eee493e4748bb5402c66c986df8dc63c0a028
CURSIST <CURSIST@KL103-71469DOC.curlokeren>
2018-03-29 10:22:18 +0200

    Merge origin/start into start
    Conflicts:
    	src/java/model/DoelstellingDAO.java

--- src/java/model/DoelstellingDAO.java
+++ src/java/model/DoelstellingDAO.java
@@ -15,18 +15,40 @@ import java.util.ArrayList;
  */
 public class DoelstellingDAO {
 
-    
-     public ArrayList<Doelstelling> doelstellingenLaden(int moduleID) {
-         
-         ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
-         
-         
-         return doelstellingen;
-     }
-    
-    
-    
-    
+    public ArrayList<Doelstelling> doelstellingenLaden(int moduleID) {
+
+        ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = "select * from doelstellingen\n"
+                    + "inner join modules on doelstellingen.moduleID=modules.moduleID\n"
+                    + "where doelstellingen.moduleID=?";
+            currentCon = ConnectionManager.getConnection();
+            ps = currentCon.prepareStatement(sql);
+
+            ps.setInt(1, moduleID);
+            ps.executeQuery(sql);
+
+            while (rs.next()) {
+
+                Doelstelling doelstelling = new Doelstelling();
+                doelstelling.setDoelstellingID(rs.getInt("doelstellingID"));
+                doelstelling.setNaam(rs.getString("naam"));
+                doelstelling.setBeschrijving(rs.getString("beschrijving"));
+                doelstelling.setKerndoelstelling(rs.getBoolean("kerndoelstelling"));
+                doelstellingen.add(doelstelling);
+            }
+
+        } catch (Exception e) {
+        } finally {
+            sluitVariabelen(rs, null, ps, currentCon);
+        }
+        return doelstellingen;
+    }
+
     //Laad alle Doelstellingen uit de Database.
     public ArrayList<Doelstelling> doelstellingenLaden(int bladz, int moduleID) {
 
