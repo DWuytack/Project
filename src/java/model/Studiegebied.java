47d9af147ad4045d8aed267b2d3ce7956598cc36
Ewout <CURSIST@KL103-71476.curlokeren>
2018-04-17 10:07:49 +0200

    Studiegebied(DAO)

---
+++ src/java/model/Studiegebied.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author CURSIST
+ */
+public class Studiegebied {
+
+    private int studiegebiedID;
+    private String naam;
+    
+    public int getStudiegebiedID() {
+        return studiegebiedID;
+    }
+
+    public void setStudiegebiedID(int studiegebiedID) {
+        this.studiegebiedID = studiegebiedID;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    
+}
