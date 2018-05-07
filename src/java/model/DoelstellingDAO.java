f46c0ed36826cdf207d577e73762d3bcca4e8c09
Ewout Phlips <CURSIST@KL103-71476.curlokeren>
2018-05-07 11:59:29 +0200

    DAO's aangepast

--- src/java/model/DoelstellingDAO.java
+++ src/java/model/DoelstellingDAO.java
@@ -15,23 +15,159 @@ import java.util.ArrayList;
  */
 public class DoelstellingDAO {
 
-    public ArrayList<Doelstelling> doelstellingenLaden(int moduleID) {
+    public void doelstellingToevoegen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
 
-        ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
+        String sql = "INSERT INTO doelstellingen (doelstellingen.naam, doelstellingen.beschrijving, doelstellingen.kerndoelstelling) VALUES (?, ?, ?)";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, doelstelling.getNaam());
+            ps.setString(2, doelstelling.getBeschrijving());
+            ps.setBoolean(3, doelstelling.getKerndoelstelling());
+
+            ps.executeQuery();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void doelstellingenToevoegen(ArrayList<Doelstelling> doelstellingen) {
+        doelstellingen.forEach((doelstelling) -> {
+            doelstellingToevoegen(doelstelling);
+        });
+    }
+
+    public void doelstellingAanpassen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE doelstellingen SET naam = ?, beschrijving = ?, kerndoelstelling = ? WHERE doelstellingID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, doelstelling.getNaam());
+            ps.setString(2, doelstelling.getBeschrijving());
+            ps.setBoolean(3, doelstelling.getKerndoelstelling());
+            ps.setInt(4, doelstelling.getDoelstellingID());
+
+            ps.executeQuery();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void doelstellingenAanpassen(ArrayList<Doelstelling> doelstellingen) {
+        doelstellingen.forEach((doelstelling) -> {
+            doelstellingAanpassen(doelstelling);
+        });
+    }
+
+    public void doelstellingVerwijderen(Doelstelling doelstelling) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+
+        String sql = "DELETE FROM doelstellingen WHERE doelstellingen.doelstellingID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setInt(1, doelstelling.getDoelstellingID());
+
+            ps.executeQuery();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void doelstellingenVerwijderen(ArrayList<Doelstelling> doelstellingen) {
+        doelstellingen.forEach((doelstelling) -> {
+            doelstellingVerwijderen(doelstelling);
+        });
+    }
+
+    public Doelstelling doelstellingLaden(String doelstellingNaam) {
+        Doelstelling doelstelling = new Doelstelling();
         Connection currentCon = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        try {
-            String sql = "select * from doelstellingen\n"
-                    + " inner join Modules on doelstellingen.moduleID=modules.moduleID\n"
-                    + " where doelstellingen.moduleID=?";
-            
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
+        String sql = "SELECT doelstellingen.* FROM doelstellingen WHERE doelstellingen.naam = ?";
 
-            ps.setInt(1, moduleID);
-            rs=ps.executeQuery();
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, doelstellingNaam);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                doelstelling.setDoelstellingID(rs.getInt("doelstellingID"));
+                doelstelling.setNaam(rs.getString("naam"));
+                doelstelling.setBeschrijving(rs.getString("beschrijving"));
+                doelstelling.setKerndoelstelling(rs.getBoolean("kerndoelstelling"));
+            }
+
+        } catch (SQLException e) {
+        } finally {
+            Utilities.sluitVariabelen(ps, rs, currentCon);
+        }
+
+        return doelstelling;
+    }
+
+    public Doelstelling doelstellingLaden(int doelstellingID) {
+        Doelstelling doelstelling = new Doelstelling();
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT doelstellingen.* FROM doelstellingen WHERE doelstellingen.doelstellingID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setInt(1, doelstellingID);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                doelstelling.setDoelstellingID(rs.getInt("doelstellingID"));
+                doelstelling.setNaam(rs.getString("naam"));
+                doelstelling.setBeschrijving(rs.getString("beschrijving"));
+                doelstelling.setKerndoelstelling(rs.getBoolean("kerndoelstelling"));
+            }
+
+        } catch (SQLException e) {
+        } finally {
+            Utilities.sluitVariabelen(ps, rs, currentCon);
+        }
+
+        return doelstelling;
+    }
+
+    public ArrayList<Doelstelling> doelstellingenLaden() {
+        ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
+        Connection currentCon = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT doelstellingen.* FROM doelstellingen";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+            statement = currentCon.createStatement();
+
+            rs = statement.executeQuery(sql);
 
             while (rs.next()) {
                 Doelstelling doelstelling = new Doelstelling();
@@ -44,226 +180,60 @@ public class DoelstellingDAO {
 
         } catch (Exception e) {
         } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
+            Utilities.sluitVariabelen(statement, rs, currentCon);
         }
+
         return doelstellingen;
     }
 
-    //Laad alle Doelstellingen uit de Database.
-    public ArrayList<Doelstelling> doelstellingenLaden(int bladz, int moduleID) {
-
-        ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
-        Connection currentCon = null;
-        Statement statement = null;
-        ResultSet rs = null;
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM doelstellingen";
-            statement = currentCon.createStatement();
-            rs = statement.executeQuery(sql);
-            int recordStart = (bladz * Instellingen.AANTAL_RECORDS_PER_PAGE) - (Instellingen.AANTAL_RECORDS_PER_PAGE - 1);
-            int recordEinde = bladz * Instellingen.AANTAL_RECORDS_PER_PAGE;
-            int recordTeller = 0;
-
-            while (rs.next()) {
-                recordTeller++;
-
-                if (recordTeller >= recordStart && recordTeller <= recordEinde) {
-                    Doelstelling doelstelling = new Doelstelling();
-                    doelstelling.setDoelstellingID(rs.getInt("doelstellingID"));
-                    doelstelling.setNaam(rs.getString("naam"));
-                    doelstelling.setBeschrijving(rs.getString("beschrijving"));
-                    doelstelling.setKerndoelstelling(rs.getBoolean("kerndoelstelling"));
-                    doelstellingen.add(doelstelling);
-                }
-            }
-
-        } catch (Exception e) {
-        } finally {
-            sluitVariabelen(rs, statement, null, currentCon);
-        }
-        return doelstellingen;
-    }
-
-    /* Geeft het aantal Doelstelligen weer. */
-    public int geefAantalDoelstellingen() {
-
-        Connection currentCon = null;
-        Statement statement = null;
-        ResultSet rs = null;
-        int aantalDoelstellingen = 0;
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT COUNT(*) FROM doelstellingen";
-            statement = currentCon.createStatement();
-
-            rs = statement.executeQuery(sql);
-            while (rs.next()) {
-                aantalDoelstellingen = rs.getInt(1);
-            }
-
-        } catch (SQLException e) {
-
-        } finally {
-            sluitVariabelen(rs, statement, null, currentCon);
-        }
-        return aantalDoelstellingen;
-    }
-
-    /* Creër een nieuwe doelstelling. */
-    public void doelstellingAanmaken(Doelstelling doelstelling) {
-        Connection currentCon = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        String sql = "INSERT INTO doelstellingen (naam, beschrijving, kerndoelstelling)\n"
-                + "VALUES (?, ?, ?)";
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-
-            ps.setString(1, "naam");
-            ps.setString(2, "beschrijving");
-            if (doelstelling.getKerndoelstelling()) {
-                ps.setInt(3, 1);
-            } else {
-                ps.setInt(3, 0);
-            }
-            ps.executeQuery();
-
-        } catch (SQLException ex) {
-        } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
-        }
-    }
-
-    /* Pas een doelstelling aan. */
-    public void doelstellingAanpassen(Doelstelling doelstelling) {
-
-        Connection currentCon = null;
-        ResultSet rs = null;
-        PreparedStatement ps = null;
-
-        String sql = "UPDATE doelstellingen SET naam = ?, beschrijving = ?, kerndoelstelling = ? WHERE doelstellingID = ?";
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-
-            ps.setString(1, doelstelling.getNaam());
-            ps.setString(2, doelstelling.getBeschrijving());
-            if (doelstelling.getKerndoelstelling()) {
-                ps.setInt(3, 1);
-            } else {
-                ps.setInt(3, 0);
-            }
-            ps.setInt(4, doelstelling.getDoelstellingID());
-            ps.executeQuery(sql);
-
-        } catch (SQLException ex) {
-        } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
-        }
-    }
-
-    /* Verwijdert een doelstelling. */
-    public void doelstellingVerwijderen(int doelstellingID) {
-
-        Connection currentCon = null;
-        PreparedStatement ps = null;
-
-        String sql
-                = "DELETE FROM doelstellingen WHERE Doelstellingen.doelstellingID = ?";
-        try {
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-
-            ps.setInt(1, doelstellingID);
-            ps.executeQuery();
-
-        } catch (SQLException ex) {
-        } finally {
-            sluitVariabelen(null, null, ps, currentCon);
-        }
-    }
-
-    /* Geeft doelstellingen weer waar een bepaalde string in zit. */
-    public ArrayList<Doelstelling> doelstellingenZoeken(String zoekterm) {
-
+    public ArrayList<Doelstelling> doelstellingenLaden(int moduleID) {
         ArrayList<Doelstelling> doelstellingen = new ArrayList<>();
         Connection currentCon = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        
+
+        String sql = "SELECT doelstellingen.* FROM doelstellingen "
+                + "INNER JOIN modules_doelstellingen ON doelstellingen.doelstellingID = modules_doelstellingen.doelstellingID "
+                + "WHERE modules_doelstellingen.moduleID = ?";
+
         try {
             currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM doelstellingen WHERE doelstellingen.naam LIKE ? OR doelstellingen.beschrijving LIKE ?";
 
             ps = currentCon.prepareStatement(sql);
-            ps.setString(1, "%" + zoekterm + "%");
-            ps.setString(2, "%" + zoekterm + "%");
+            ps.setInt(1, moduleID);
 
             rs = ps.executeQuery();
 
             while (rs.next()) {
                 Doelstelling doelstelling = new Doelstelling();
-                    doelstelling.setDoelstellingID(rs.getInt("doelstellingID"));
-                    doelstelling.setNaam(rs.getString("naam"));
-                    doelstelling.setBeschrijving(rs.getString("beschrijving"));
-                    doelstelling.setKerndoelstelling(rs.getBoolean("kerndoelstelling"));
-                    doelstellingen.add(doelstelling);
+                doelstelling.setDoelstellingID(rs.getInt("doelstellingID"));
+                doelstelling.setNaam(rs.getString("naam"));
+                doelstelling.setBeschrijving(rs.getString("beschrijving"));
+                doelstelling.setKerndoelstelling(rs.getBoolean("kerndoelstelling"));
+                doelstellingen.add(doelstelling);
             }
+
         } catch (Exception e) {
         } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
+            Utilities.sluitVariabelen(ps, rs, currentCon);
         }
+
         return doelstellingen;
     }
 
-    /* Sluit enkele variabelen en zet ze op null */
-    private void sluitVariabelen(ResultSet rs, Statement statement, PreparedStatement ps, Connection currentCon) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (Exception e) {
-            }
-            try {
-                rs = null;
-            } catch (Exception e) {
-            }
-        }
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (Exception e) {
-            }
-            try {
-                statement = null;
-            } catch (Exception e) {
-            }
-        }
-        if (ps != null) {
-            try {
-                ps.close();
-            } catch (Exception e) {
-            }
-            try {
-                ps = null;
-            } catch (Exception e) {
-            }
-        }
-        if (currentCon != null) {
-            try {
-                currentCon.close();
-            } catch (Exception e) {
-            }
-            try {
-                currentCon = null;
-            } catch (Exception e) {
-            }
-        }
+    public String geefDoelstellingNaam(int doelstellingID) {
+        return doelstellingLaden(doelstellingID).getNaam();
+    }
+
+    public int geefDoelstellingID(String doelstellingNaam) {
+        return doelstellingLaden(doelstellingNaam).getDoelstellingID();
+    }
+    
+    public int geefAantalDoelstellingen() {
+        return doelstellingenLaden().size();
+    }
+    
+    public int geefAantalDoelstellingen(int moduleID) {
+        return doelstellingenLaden(moduleID).size();
     }
 }
--- src/java/model/ModuleDAO.java
+++ src/java/model/ModuleDAO.java
@@ -8,103 +8,142 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
- * ModuleDAO(Module Data Access Object) is een klasse voor alle
- * handelingen in de database betreffend Modules.
+ * ModuleDAO(Module Data Access Object) is een klasse voor alle handelingen in
+ * de database betreffend Modules.
  *
  * @author Ewout Phlips
  */
 public class ModuleDAO {
 
     public void moduleToevoegen(Module module) {
-
-        //connectie maken met database
-        Connection connectie = null;
-
-        //insertquery
-        String sql = "INSERT INTO modules "
-                + "(moduleID,naam) VALUES (?,?)";
-
-        //opslaan in database
-        try {
-            PreparedStatement statement = connectie.prepareStatement(sql);
-            statement.setInt(1, module.getModuleID());
-            statement.setString(2, module.getNaam());
-            statement.execute();
-            statement.close();
-            connectie.close();
-
-        } catch (SQLException e) {
-
-        }
-    }
-
-    public void moduleAanpassen(Module module) {
-
-        //connectie maken met database
-        Connection connectie = null;
-        connectie = ConnectionManager.getConnection();
-
-        //insertquery
-        String sql = "UPDATE modules "
-                + " set naam =  " + module.getNaam() + " where moduleID= "
-                + module.getModuleID();
-
-        //opslaan in database
-        try {
-            Statement statement = connectie.createStatement();
-            statement.executeUpdate(sql);
-            statement.close();
-            connectie.close();
-
-        } catch (SQLException e) {
-
-        }
-
-    }
-
-    public void moduleVerwijderen(Module module) {
-
         Connection currentCon = null;
-        Statement statement = null;
+        PreparedStatement ps = null;
 
-        String sql = "DELETE FROM modules "
-                + " WHERE moduleID= " + module.getModuleID();
+        String sql = "INSERT INTO modules (modules.naam) VALUES (?)";
 
         try {
             currentCon = ConnectionManager.getConnection();
-            statement = currentCon.createStatement();
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
 
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, module.getNaam());
+
+            ps.executeQuery();
+        } catch (SQLException ex) {
         } finally {
-            sluitVariabelen(null, statement, null, currentCon);
+            Utilities.sluitVariabelen(ps, currentCon);
         }
     }
 
-    public Module moduleLaden(int moduleID) {
+    public void modulesToevoegen(ArrayList<Module> modules) {
+        modules.forEach((module) -> {
+            moduleToevoegen(module);
+        });
+    }
 
+    public void moduleAanpassen(Module module) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE modules SET naam = ? WHERE moduleID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, module.getNaam());
+            ps.setInt(2, module.getModuleID());
+
+            ps.executeQuery();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void modulesAanpassen(ArrayList<Module> modules) {
+        modules.forEach((module) -> {
+            moduleAanpassen(module);
+        });
+    }
+
+    public void moduleVerwijderen(Module module) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+
+        String sql = "DELETE FROM modules WHERE modules.moduleID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setInt(1, module.getModuleID());
+
+            ps.executeQuery();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void modulesVerwijderen(ArrayList<Module> modules) {
+        modules.forEach((module) -> {
+            moduleVerwijderen(module);
+        });
+    }
+
+    public Module moduleLaden(String moduleNaam) {
+        Module module = new Module();
         Connection currentCon = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        String sql = "SELECT * FROM modules WHERE modules.moduleID = ?;";
+        String sql = "SELECT modules.* FROM modules WHERE modules.naam = ?";
+
         try {
             currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, moduleNaam);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                module.setModuleID(rs.getInt("moduleID"));
+                module.setNaam(rs.getString("naam"));
+            }
+
+        } catch (SQLException e) {
+        } finally {
+            Utilities.sluitVariabelen(ps, rs, currentCon);
+        }
+
+        return module;
+    }
+
+    public Module moduleLaden(int moduleID) {
+        Module module = new Module();
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT modules.* FROM modules WHERE modules.moduleID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
             ps = currentCon.prepareStatement(sql);
             ps.setInt(1, moduleID);
+
             rs = ps.executeQuery();
-
-            Module module = new Module();
-            module.setModuleID(rs.getInt("opleidingID"));
-            module.setNaam(rs.getString("naam"));
-
-            return module;
-        } catch (Exception e) {
-
+            while (rs.next()) {
+                module.setModuleID(rs.getInt("moduleID"));
+                module.setNaam(rs.getString("naam"));
+            }
+        } catch (SQLException e) {
         } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
+            Utilities.sluitVariabelen(ps, rs, currentCon);
         }
-        return null;
+
+        return module;
     }
 
     public ArrayList<Module> modulesLaden() {
@@ -113,161 +152,73 @@ public class ModuleDAO {
         Statement statement = null;
         ResultSet rs = null;
 
+        String sql = "SELECT modules.* FROM modules";
+
         try {
             currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM modules;";
             statement = currentCon.createStatement();
+
             rs = statement.executeQuery(sql);
 
             while (rs.next()) {
                 Module module = new Module();
                 module.setModuleID(rs.getInt("moduleID"));
                 module.setNaam(rs.getString("naam"));
-
                 modules.add(module);
             }
-        } catch (SQLException e) {
 
+        } catch (Exception e) {
         } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-                rs = null;
-            }
-
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (Exception e) {
-
-                }
-
-                statement = null;
-            }
-
-            if (currentCon != null) {
-                try {
-                    currentCon.close();
-                } catch (Exception e) {
-
-                }
-
-                currentCon = null;
-            }
-
+            Utilities.sluitVariabelen(statement, rs, currentCon);
         }
+
         return modules;
     }
 
     public ArrayList<Module> modulesLaden(int opleidingID) {
-
         ArrayList<Module> modules = new ArrayList<>();
         Connection currentCon = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
 
+        String sql = "SELECT modules.* FROM modules "
+                + "INNER JOIN opleidingen_modules ON modules.moduleID = opleidingen_modules.moduleID "
+                + "WHERE opleidingen_modules.opleidingID = ?";
+
         try {
             currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT modules.naam, modules.moduleID FROM modules"
-                    + " inner join opleidingen_modules on opleidingen_modules.moduleID = modules.moduleID "
-                    + " inner join opleidingen on opleidingen_modules.opleidingID = opleidingen.opleidingID "
-                    + " where opleidingen_modules.opleidingID = ? ";
 
             ps = currentCon.prepareStatement(sql);
             ps.setInt(1, opleidingID);
+
             rs = ps.executeQuery();
 
             while (rs.next()) {
                 Module module = new Module();
+                module.setModuleID(rs.getInt("doelstellingID"));
                 module.setNaam(rs.getString("naam"));
-                module.setModuleID(rs.getInt("moduleID"));
                 modules.add(module);
             }
-        } catch (SQLException e) {
-
+        } catch (Exception e) {
         } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
+            Utilities.sluitVariabelen(ps, rs, currentCon);
         }
         return modules;
     }
 
-    public ArrayList<Module> modulesLaden(String opleidingNaam) {
-        OpleidingDAO opleidingDAO = new OpleidingDAO();
-
-        return modulesLaden(opleidingDAO.geefOpleidingID(opleidingNaam));
+    public String geefModuleNaam(int moduleID) {
+        return moduleLaden(moduleID).getNaam();
     }
 
     public int geefModuleID(String moduleNaam) {
-
-        int moduleID = 0;
-        Connection currentCon = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            String sql = "select moduleID from modules where "
-                    + "modules.naam= ?";
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-            ps.setString(1, moduleNaam);
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                moduleID = rs.getInt("moduleID");
-            }
-
-        } catch (Exception e) {
-
-        } finally {
-            sluitVariabelen(rs, ps, null, currentCon);
-        }
-
-        return moduleID;
+        return moduleLaden(moduleNaam).getModuleID();
     }
 
-    /* Sluit enkele variabelen en zet ze op null */
-    private void sluitVariabelen(ResultSet rs, Statement statement, PreparedStatement ps, Connection currentCon) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (Exception e) {
-            }
-            try {
-                rs = null;
-            } catch (Exception e) {
-            }
-        }
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (Exception e) {
-            }
-            try {
-                statement = null;
-            } catch (Exception e) {
-            }
-        }
-        if (ps != null) {
-            try {
-                ps.close();
-            } catch (Exception e) {
-            }
-            try {
-                ps = null;
-            } catch (Exception e) {
-            }
-        }
-        if (currentCon != null) {
-            try {
-                currentCon.close();
-            } catch (Exception e) {
-            }
-            try {
-                currentCon = null;
-            } catch (Exception e) {
-            }
-        }
+    public int geefAantalModules() {
+        return modulesLaden().size();
+    }
+
+    public int geefAantalModules(int opleidingID) {
+        return modulesLaden(opleidingID).size();
     }
 }
