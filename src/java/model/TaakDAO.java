a9b3a1c7b0f0e51aa5d6215a7db0cafd0cb96d6e
EwoutPhlips <EwoutPhlips@192.168.0.200>
2018-05-10 22:04:43 +0200

    TaakDAO geüpdated.

--- src/java/model/TaakDAO.java
+++ src/java/model/TaakDAO.java
@@ -8,24 +8,151 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
- * Deze klasse is verantwoordelijk voor alle gebruikersbewerkingen in de
- * database.
+ * TaakDAO(Taak Data Access Object) is een klasse voor alle
+ * handelingen in de database betreffend Taken.
  *
- * @author Gil
+ * @author Ewout Phlips
  */
 public class TaakDAO {
 
-    public ArrayList<Taak> takenLaden(int bladz) {
+    public void taakToevoegen(Taak taak) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
 
+        String sql = "INSERT INTO taken (taken.naam, taken.beschrijving) VALUES (?, ?)";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, taak.getNaam());
+            ps.setString(2, taak.getBeschrijving());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void takenToevoegen(ArrayList<Taak> taken) {
+        taken.forEach((taak) -> this.taakToevoegen(taak));
+    }
+
+    public void taakAanpassen(Taak taak) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE taken SET naam = ?, beschrijving = ? WHERE taakID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, taak.getNaam());
+            ps.setString(2, taak.getBeschrijving());
+            ps.setInt(3, taak.getTaakID());
+
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void takenAanpassen(ArrayList<Taak> taken) {
+        taken.forEach((taak) -> this.taakAanpassen(taak));
+    }
+
+    public void taakVerwijderen(Taak taak) {
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+
+        String sql = "DELETE FROM taken WHERE taken.taakID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setInt(1, taak.getTaakID());
+
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+        } finally {
+            Utilities.sluitVariabelen(ps, currentCon);
+        }
+    }
+
+    public void takenVerwijderen(ArrayList<Taak> taken) {
+        taken.forEach((taak) -> this.taakVerwijderen(taak));
+    }
+
+    public Taak taakLaden(String taakNaam) {
+        Taak taak = new Taak();
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT taken.* FROM taken WHERE taken.naam = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setString(1, taakNaam);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                taak.setTaakID(rs.getInt("taakID"));
+                taak.setNaam(rs.getString("naam"));
+                taak.setBeschrijving(rs.getString("beschrijving"));
+            }
+        } catch (SQLException e) {
+        } finally {
+            Utilities.sluitVariabelen(ps, rs, currentCon);
+        }
+        return taak;
+    }
+
+    public Taak taakLaden(int taakID) {
+        Taak taak = new Taak();
+        Connection currentCon = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT taken.* FROM taken WHERE taken.taakID = ?";
+
+        try {
+            currentCon = ConnectionManager.getConnection();
+
+            ps = currentCon.prepareStatement(sql);
+            ps.setInt(1, taakID);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                taak.setTaakID(rs.getInt("taakID"));
+                taak.setNaam(rs.getString("naam"));
+                taak.setBeschrijving(rs.getString("beschrijving"));
+            }
+        } catch (SQLException e) {
+        } finally {
+            Utilities.sluitVariabelen(ps, rs, currentCon);
+        }
+        return taak;
+    }
+
+    public ArrayList<Taak> takenLaden() {
         ArrayList<Taak> taken = new ArrayList<>();
         Connection currentCon = null;
         Statement statement = null;
         ResultSet rs = null;
 
+        String sql = "SELECT taken.* FROM taken";
+
         try {
             currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM taken";
             statement = currentCon.createStatement();
+
             rs = statement.executeQuery(sql);
 
             while (rs.next()) {
@@ -36,302 +163,58 @@ public class TaakDAO {
                 taken.add(taak);
             }
         } catch (SQLException e) {
-
         } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-                rs = null;
-            }
-
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (Exception e) {
-
-                }
-
-                statement = null;
-            }
-
-            if (currentCon != null) {
-                try {
-                    currentCon.close();
-                } catch (Exception e) {
-
-                }
-
-                currentCon = null;
-            }
-
+            Utilities.sluitVariabelen(statement, rs, currentCon);
         }
         return taken;
     }
 
-    public int geefAantalTaken() {
-
-        Connection currentCon = null;
-        Statement statement = null;
-        ResultSet rs = null;
-        int aantalTaken = 0;
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            String sql = "select COUNT(*) from taken";
-            statement = currentCon.createStatement();
-
-            rs = statement.executeQuery(sql);
-            while (rs.next()) {
-                aantalTaken = rs.getInt(1);
-            }
-
-        } catch (SQLException e) {
-
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-                rs = null;
-            }
-
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (Exception e) {
-
-                }
-
-                statement = null;
-            }
-
-            if (currentCon != null) {
-                try {
-                    currentCon.close();
-                } catch (Exception e) {
-
-                }
-
-                currentCon = null;
-            }
-
-        }
-        return aantalTaken;
-    }
-
-    public void taakAanmaken(Taak taak) {
-        Connection currentCon = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        String sql = "INSERT INTO taken (naam,beschrijving) VALUES (?,?)";
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-
-            ps.setString(1, taak.getNaam());
-            ps.setString(2, taak.getBeschrijving());
-            ps.executeQuery(sql);
-
-        } catch (SQLException ex) {
-
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-
-                }
-                rs = null;
-            }
-
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-
-                }
-                ps = null;
-            }
-
-            if (currentCon != null) {
-                try {
-                    currentCon.close();
-                } catch (SQLException e) {
-
-                }
-
-                currentCon = null;
-            }
-        }
-    }
-
-    public void taakAanpassen(int taakID, Taak taak) {
-        Connection currentCon = null;
-        ResultSet rs = null;
-        PreparedStatement ps = null;
-
-        String sql = "UPDATE taken(taakID, naam, beschrijving) VALUES(?,?,?)";
-
-        try {
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-
-            ps.setString(1, taak.getNaam());
-            ps.setString(2, taak.getBeschrijving());
-            ps.executeQuery(sql);
-
-        } catch (SQLException ex) {
-
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-
-                }
-                rs = null;
-            }
-
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-
-                }
-                ps = null;
-            }
-
-            if (currentCon != null) {
-                try {
-                    currentCon.close();
-                } catch (SQLException e) {
-
-                }
-
-                currentCon = null;
-            }
-        }
-
-    }
-
-    public ArrayList<Taak> takenZoeken(String zoekterm) {
-
+    public ArrayList<Taak> takenLaden(int doelstellingID) {
         ArrayList<Taak> taken = new ArrayList<>();
         Connection currentCon = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
 
+        String sql = "SELECT taken.* FROM taken "
+                + "INNER JOIN doelstellingen_taken ON taken.taakID = doelstellingen_taken.taakID "
+                + "WHERE doelstellingen_taken.taakID = ?";
+
         try {
             currentCon = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM taken  WHERE taken.naam LIKE ? OR taken.beschrijving LIKE ?";
 
             ps = currentCon.prepareStatement(sql);
-            ps.setString(1, "%" + zoekterm + "%");
-            ps.setString(2, "%" + zoekterm + "%");
+            ps.setInt(1, doelstellingID);
 
             rs = ps.executeQuery();
 
             while (rs.next()) {
                 Taak taak = new Taak();
-                taak.setTaakID(rs.getInt("taakID"));
-                taak.setBeschrijving(rs.getString("beschrijving"));
+                taak.setTaakID(rs.getInt("doelstellingID"));
                 taak.setNaam(rs.getString("naam"));
+                taak.setBeschrijving(rs.getString("beschrijving"));
                 taken.add(taak);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
         } finally {
-            sluitVariabelen(rs, null, ps, currentCon);
+            Utilities.sluitVariabelen(ps, rs, currentCon);
         }
         return taken;
     }
 
-    private void sluitVariabelen(ResultSet rs, Statement statement, PreparedStatement ps, Connection currentCon) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (Exception e) {
-            }
-            try {
-                rs = null;
-            } catch (Exception e) {
-            }
-        }
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (Exception e) {
-            }
-            try {
-                statement = null;
-            } catch (Exception e) {
-            }
-        }
-        if (ps != null) {
-            try {
-                ps.close();
-            } catch (Exception e) {
-            }
-            try {
-                ps = null;
-            } catch (Exception e) {
-            }
-        }
-        if (currentCon != null) {
-            try {
-                currentCon.close();
-            } catch (Exception e) {
-            }
-            try {
-                currentCon = null;
-            } catch (Exception e) {
-            }
-        }
+    public String geefTaakNaam(int taakID) {
+        return taakLaden(taakID).getNaam();
     }
 
-    public void takenVerwijderen(int taakID) {
+    public int geefTaakID(String taakNaam) {
+        return taakLaden(taakNaam).getTaakID();
+    }
 
-        Connection currentCon = null;
-        PreparedStatement ps = null;
+    public int geefAantalTaken() {
+        return takenLaden().size();
+    }
 
-        String sql
-                = "DELETE from taken where taken.taakID = ?";
-        try {
-            currentCon = ConnectionManager.getConnection();
-            ps = currentCon.prepareStatement(sql);
-
-            ps.setInt(1, taakID);
-            ps.executeQuery();
-
-        } catch (SQLException ex) {
-
-            System.out.println(ex.getMessage());
-
-        } finally {
-
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-
-                }
-                ps = null;
-            }
-
-            if (currentCon != null) {
-                try {
-                    currentCon.close();
-                } catch (SQLException e) {
-
-                }
-
-                currentCon = null;
-            }
-        }
+    public int geefAantalTaken(int doelstellingID) {
+        return takenLaden(doelstellingID).size();
     }
 
 }
