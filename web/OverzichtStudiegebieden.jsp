61e3e9725dd2f95db105c9a85973488c0f9e1fda
Ewout Phlips <CURSIST@KL103-71476.curlokeren>
2018-06-05 11:14:14 +0200

    StudiegebiedenOverzicht FINAL VERSION.

--- web/OverzichtStudiegebieden.jsp
+++ web/OverzichtStudiegebieden.jsp
@@ -1,8 +1,8 @@
 <%-- 
     Document   : OverzichtStudiegebieden
     Created on : 30-mrt-2018, 11:10:04
-    Author     : CURSIST
---%>
+    Author     : Davino Tizarine
+
 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>  
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
@@ -76,5 +76,62 @@
                 <td>  <label id="studiegebied"  hidden  style="max-width:120px;">
                     </label> </td>
         </form>
+    </body>
+</html>
+--%>
+
+<%-- HUIDIG WERKEND STUK -----------------------------------------------------------
+    Document   : OverzichtStudiegebieden
+    Created on : 22-mei-2018, 9:50:00
+    Author     : Ewout
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="keywords" content="ScoreOnWeb, Studiegebieden"
+              name="description" content="Overzicht van Studiegebieden"
+              name="author" content="Ewout Phlips"
+              name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="js/nav.js"></script>
+        <script src="js/studiegebieden.js"></script>
+        <link rel="stylesheet" href="css/theme.css">
+        <link rel="stylesheet" href="css/studiegebiedenOverzicht.css">
+        <title>ScoreOnWeb - Overzicht Studiegebieden</title>    
+    </head>
+
+    <body>
+        <!-- BOVENBALK -->
+        <%@include file="Bovenbalk.jsp" %>
+
+        <!-- PAGINA TITEL -->
+        <section id="pagename">
+            <h2>Overzicht Studiegebieden</h2> 
+        </section>
+
+        <!-- TABEL OVERZICHT -->
+        <br>
+        <section>
+            <div class="table-container" id="overzicht">
+                <table class="studiegebieden">
+                    <thead>
+                        <tr>
+                            <th>Studiegebieden</th>
+                        </tr>
+                    </thead>
+                    <c:forEach items="${studiegebieden}" var="studiegebied">
+                        <tr class="studiegebied" data-ID="${studiegebied.studiegebiedID}" >
+                            <td><button onclick="toggle(this)">+</button>${studiegebied.naam}
+                            </td>
+                        </tr>
+                    </c:forEach>
+                </table>
+            </div>
+        </section>
+
     </body>
 </html>
--- web/js/studiegebieden.js
+++ web/js/studiegebieden.js
@@ -1,74 +1,78 @@
 function toggle(Button) {
-    var Klasse = Button.parentElement.parentElement.className;
-    if (Klasse != "taak") {
-        if (Button.textContent === "+") {   // LAAD ALLE OPLEIDINGEN IN TABEL
+    var klasse = Button.parentElement.parentElement.className;
+    if (Button.textContent === "+") {
 
-            var xhttp = new XMLHttpRequest();
+        var xhttp = new XMLHttpRequest();
 
-            if (window.XMLHttpRequest) {
-                // code voor moderne browsers
-                xhttp = new XMLHttpRequest();
-            } else {
-                // code voor oude IE browsers
-                xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            //open(method,url,async)
-            var ID = Button.parentElement.parentElement.dataset.id;
-            xhttp.open("POST", "StudiegebiedenServlet?" + Klasse + "=" + ID, true);
-            xhttp.send();
-            xhttp.onreadystatechange = function () {
-
-                //200: "OK"
-                //403: "Forbidden"
-                //404: "Not Found"
-
-                //0: request not initialized 
-                //1: server connection established
-                //2: request received 
-                //3: processing request 
-                //4: request finished and response is ready
-
-                if (this.readyState === 4 && this.status === 200) {
-
-                    const data = JSON.parse(xhttp.responseText);
-                    table = document.createElement('table');
-
-                    for (let i = 0; i < data.length; i++) {
-
-                        button = document.createElement('button');
-                        button.setAttribute("onclick", "toggle(this)");
-                        button.textContent = "+";
-
-                        td = document.createElement('td');
-                        textNode = document.createTextNode(data[i].naam);
-
-                        tr = document.createElement('tr');
-                        tr.dataset.id = data[i].opleidingID;
-                        if (Klasse === "studiegebied") {
-                            tr.className = "opleiding";
-                            td.appendChild(button);
-                        } else if (Klasse === "opleiding") {
-                            tr.className = "module";
-                            td.appendChild(button);
-                        } else if (Klasse === "module") {
-                            tr.className = "doelstelling";
-                            td.appendChild(button);
-                        } else if (Klasse === "doelstellig") {
-                            tr.className = "taak";
-                        }
-
-                        
-                        td.appendChild(textNode);
-                        tr.appendChild(td);
-                        table.appendChild(tr);
-                    }
-                    Button.textContent = "-";
-                    Button.parentElement.appendChild(table);
-                }
-            };
-        } else if (Button.textContent === "-") {
-            Button.parentElement.removeChild(Button.parentElement.childNodes[2]);
-            Button.textContent = "+";
+        if (window.XMLHttpRequest) {
+            // code voor moderne browsers
+            xhttp = new XMLHttpRequest();
+        } else {
+            // code voor oude IE browsers
+            xhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+        //open(method,url,async)
+        var ID = Button.parentElement.parentElement.dataset.id;
+        xhttp.open("POST", "StudiegebiedenServlet?" + klasse + "=" + ID, true);
+        xhttp.send();
+        xhttp.onreadystatechange = function () {
+
+            //200: "OK"
+            //403: "Forbidden"
+            //404: "Not Found"
+
+            //0: request not initialized 
+            //1: server connection established
+            //2: request received 
+            //3: processing request 
+            //4: request finished and response is ready
+
+            if (this.readyState === 4 && this.status === 200) {
+
+                const data = JSON.parse(xhttp.responseText);
+                table = document.createElement('table');
+                tbody = document.createElement('tbody');
+
+                table.appendChild(tbody);
+
+                for (let i = 0; i < data.length; i++) {
+
+                    button = document.createElement('button');
+                    button.setAttribute("onclick", "toggle(this)");
+                    button.textContent = "+";
+
+                    td = document.createElement('td');
+                    textNode = document.createTextNode(data[i].naam);
+
+                    tr = document.createElement('tr');
+
+                    if (klasse === "studiegebied") {
+                        tr.className = "opleiding";
+                        tr.dataset.id = data[i].opleidingID;
+                        td.appendChild(button);
+                    } else if (klasse === "opleiding") {
+                        tr.className = "module";
+                        tr.dataset.id = data[i].moduleID;
+                        td.appendChild(button);
+                    } else if (klasse === "module") {
+                        tr.className = "doelstelling";
+                        tr.dataset.id = data[i].doelstellingID;
+                        td.appendChild(button);
+                    } else if (klasse === "doelstelling") {
+                        tr.className = "taak";
+                    }
+
+                    td.appendChild(textNode);
+                    tr.appendChild(td);
+                    tbody.appendChild(tr);
+                }
+                Button.textContent = "-";
+                Button.parentElement.appendChild(table);
+            }
+        };
+
+    } else if (Button.textContent === "-") {
+        Button.parentElement.removeChild(Button.parentElement.childNodes[2]);
+        Button.textContent = "+";
     }
 }
