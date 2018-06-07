bd06e73453a73f116904e9f1a14c2f25a3b3d82a
gil-_ <gil-_@DESKTOP-MJGVATI>
2018-06-07 20:37:53 +0200

    cursisten & doelstellingen worden geladen

--- src/java/model/ScoreDAO.java
+++ src/java/model/ScoreDAO.java
@@ -20,18 +20,15 @@ public class ScoreDAO {
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        String sql = "SELECT doelstellingen.naam, avg(beoordelingssoorten.waarde) AS score FROM doelstellingen_taken\n" +
-        "INNER JOIN doelstellingen ON doelstellingen_taken.doelstellingID = doelstellingen.doelstellingID\n" +
-        "INNER JOIN taken ON doelstellingen_taken.taakID = taken.taakID\n" +
-        "INNER JOIN modules_taken ON taken.taakID = modules_taken.taakID\n" +
-        "INNER JOIN modules on modules_taken.moduleID = modules.moduleID\n" +
-        "INNER JOIN evalform_scores ON taken.taakID = evalform_scores.taakID\n" +
-        "INNER JOIN beoordelingssoorten ON evalform_scores.beoordelingssoortID = beoordelingssoorten.beoordelingssoortID\n" +
-        "INNER JOIN evaluatieformulieren ON evalform_scores.evaluatieformID = evaluatieformulieren.evaluatieformID\n" +
-        "INNER JOIN inschrijvingen ON evaluatieformulieren.inschrijvingID = inschrijvingen.inschrijvingID\n" +
-        "INNER JOIN gebruikers ON inschrijvingen.gebruikerID = gebruikers.gebruikerID\n" +
-        "WHERE gebruikers.gebruikerID = ? AND modules.moduleID = ?\n" +
-        "GROUP BY doelstellingen.naam";
+        String sql = "SELECT gebruikers.voornaam, gebruikers.achternaam, AVG(beoordelingssoorten.waarde) as score FROM evaluatieformulieren\n"
+                + "INNER JOIN inschrijvingen ON evaluatieformulieren.inschrijvingID = inschrijvingen.inschrijvingID\n"
+                + "INNER JOIN gebruikers ON inschrijvingen.gebruikerID = gebruikers.gebruikerID\n"
+                + "INNER JOIN evalform_scores ON evaluatieformulieren.evaluatieformID = evalform_scores.evaluatieformID\n"
+                + "INNER JOIN beoordelingssoorten ON evalform_scores.beoordelingssoortID = beoordelingssoorten.beoordelingssoortID\n"
+                + "WHERE inschrijvingen.moduleID=? "
+                + "AND inschrijvingen.semesterID=? "
+                + "AND inschrijvingen.schooljaarID=? "
+                + "GROUP BY gebruikers.gebruikerID;";
 
         try {
             //connectie met de database
@@ -65,16 +62,18 @@ public class ScoreDAO {
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        String sql = "SELECT doelstellingen.naam, avg(beoordelingssoorten.waarde) AS score FROM doelstellingen_taken "
-                + "INNER JOIN doelstellingen ON doelstellingen_taken.doelstellingID = doelstellingen.doelstellingID "
-                + "INNER JOIN taken ON doelstellingen_taken.taakID = taken.taakID "
-                + "LEFT JOIN evalform_scores ON taken.taakID = evalform_scores.taakID "
-                + "INNER JOIN beoordelingssoorten ON evalform_scores.beoordelingssoortID = beoordelingssoorten.beoordelingssoortID "
-                + "INNER JOIN evaluatieformulieren ON evalform_scores.evaluatieformID = evaluatieformulieren.evaluatieformID "
-                + "INNER JOIN inschrijvingen ON evaluatieformulieren.inschrijvingID = inschrijvingen.inschrijvingID "
-                + "INNER JOIN gebruikers ON inschrijvingen.gebruikerID = gebruikers.gebruikerID "
-                + "WHERE gebruikers.gebruikerID = ? AND modules.moduleID = ? "
-                + "GROUP BY doelstellingen.naam";
+        String sql = "SELECT doelstellingen.naam, avg(beoordelingssoorten.waarde) AS score FROM doelstellingen_taken\n" +
+        "right JOIN doelstellingen ON doelstellingen_taken.doelstellingID = doelstellingen.doelstellingID\n" +
+        "INNER JOIN taken ON doelstellingen_taken.taakID = taken.taakID\n" +
+        "INNER JOIN modules_taken ON taken.taakID = modules_taken.taakID\n" +
+        "INNER JOIN modules on modules_taken.moduleID = modules.moduleID\n" +
+        "INNER JOIN evalform_scores ON taken.taakID = evalform_scores.taakID\n" +
+        "INNER JOIN beoordelingssoorten ON evalform_scores.beoordelingssoortID = beoordelingssoorten.beoordelingssoortID\n" +
+        "INNER JOIN evaluatieformulieren ON evalform_scores.evaluatieformID = evaluatieformulieren.evaluatieformID\n" +
+        "INNER JOIN inschrijvingen ON evaluatieformulieren.inschrijvingID = inschrijvingen.inschrijvingID\n" +
+        "INNER JOIN gebruikers ON inschrijvingen.gebruikerID = gebruikers.gebruikerID\n" +
+        "WHERE gebruikers.gebruikerID = ? AND modules.moduleID = ?\n" +
+        "GROUP BY doelstellingen.naam";
 
         try {
             //connectie met de database
